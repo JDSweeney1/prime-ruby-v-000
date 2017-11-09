15904aaae50af4cc1497a6bc8c348b37212bd5e9
Jered Sweeney <sweeneyjered@gmail.com>
2017-11-09 01:37:42 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,28 @@
-require 'prime'
+def sieve(max)
 
-def prime?(i)
-  Prime.prime?(i)
+  primes = (0..max).to_a
+
+  primes[0] = primes[1] = nil
+
+
+  counter = 0
+  primes.each do |p|
+
+    next unless p
+
+
+    break if p*p > max
+    counter += 1
+
+    (p*p).step(max,p) { |m| primes[m] = nil }
+  end
+
+
+  puts "Solved for #{max} in #{counter} steps."
+  primes.compact
 end
+
+def prime?(num)
+  sieve(num).include?(num)
+end
+
