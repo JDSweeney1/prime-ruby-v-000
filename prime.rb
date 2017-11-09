6949fb8e741680c9de1404945e4011c8c4b69989
Jered Sweeney <sweeneyjered@gmail.com>
2017-11-09 01:41:56 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,19 +2,13 @@ def primes(i)
 
   primes = (0..i).to_a
   primes[0] = primes[1] = nil
-
-
   counter = 0
   primes.each do |int|
     next unless int
     break if int > i
     counter += 1
-
     (int*int).step(i,int) { |m| primes[m] = nil }
   end
-
-
-  puts "Solved for #{i} in #{counter} steps."
   primes.compact
 end
 
